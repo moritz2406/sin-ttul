f240bc0ba2b8ea3e711142c183d61a40e631e448
moritz2406 <122538931+moritz2406@users.noreply.github.com>
2023-01-20 12:23:19 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 10; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(100)
+    }
+})
+led.enable(false)
 basic.forever(function () {
 	
 })
